2bb1cfa34a2b6c70dc7a384527ad7714589bdd3d
Carlos Sierra Doncel <C.SierraDoncel@gmail.com>
2022-09-26 11:02:20 +0200

    .

---
+++ nextround.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, k, i, aux = 0;
+	int score[50];
+
+	scanf("%d %d", &n, &k);
+
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d ", score + i);
+	}
+
+	while(score[aux] >= score[k - 1] && score[aux] > 0 && aux < n){
+		aux++;
+	}
+
+	printf("%d\n", aux);
+	return 0;
+}
