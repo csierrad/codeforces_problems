61f477fdb185a46b0e4c7d7bd7c007259552a562
Carlos Sierra Doncel <C.SierraDoncel@gmail.com>
2022-11-22 12:45:37 +0100

    2022.11.22

---
+++ trafficlights.c
@@ -0,0 +1,34 @@
+/*
+Traffic Lights
+https://codeforces.com/contest/1744/problem/C
+1000
+*/
+
+#include <stdio.h>
+
+int main(){
+
+	int i, j, t, n, max, cnt;
+	char c, s[200001];
+
+	scanf("%d", &t);
+
+	while(t--){
+		scanf("%d %c %s", &n, &c, s);
+
+		for(i = max = 0; i < n; i++){
+			if(s[i] == c){
+				for(j = cnt = 0; s[i + j] != 'g'; j++, cnt++){
+					if((j + i) == (n - 1)) j = -i - 1;
+				}
+
+				if(cnt > max) max = cnt;
+				i += cnt;
+			}
+		}
+
+		printf("%d\n", max);
+	}
+
+	return 0;
+}
